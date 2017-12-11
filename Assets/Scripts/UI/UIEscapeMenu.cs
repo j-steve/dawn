9d432a0e6df03c32ad62811bd2c7a7fa9282443d
jsteve <steve5805@gmail.com>
2017-12-10 16:52:42 -0800

    Renames EscapeMenu, and moves into DawnX.UI namespace.

---
+++ Assets/Scripts/UI/UIEscapeMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DawnX.UI
+{
+    public class UIEscapeMenu : MonoBehaviour
+    {
+
+        public Canvas GuiMenuCanvas;
+        public UnityEngine.UI.Button ReturnToGameButton;
+        public UnityEngine.UI.Button QuitToDesktopButton;
+        public UnityEngine.UI.Button SaveButton;
+
+        // Use this for initialization
+        void Start()
+        {
+            HideMenu();
+            ReturnToGameButton.onClick.AddListener(HideMenu);
+            QuitToDesktopButton.onClick.AddListener(QuitToDesktop);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButtonDown("Cancel")) {
+                ToggleMenu(!GuiMenuCanvas.gameObject.activeInHierarchy);
+            }
+        }
+
+        void ToggleMenu(bool doShow)
+        {
+            GuiMenuCanvas.gameObject.SetActive(doShow);
+        }
+
+        void HideMenu()
+        {
+            ToggleMenu(false);
+        }
+
+        void QuitToDesktop()
+        {
+            Application.Quit();
+        }
+    }
+}
--- Assets/Scripts/UI/UILoadingOverlay.cs
+++ Assets/Scripts/UI/UILoadingOverlay.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using System;
 
 namespace DawnX.UI
 {
