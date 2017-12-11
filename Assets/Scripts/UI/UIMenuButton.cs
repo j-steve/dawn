13a561d0485d514a16114477b312f3eeeecdb004
jsteve <steve5805@gmail.com>
2017-12-10 18:04:04 -0800

    Adds better EscapeMenu buttons, with backgound images and hover/click colorchange effects.

---
+++ Assets/Scripts/UI/UIMenuButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIMenuButton : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField] Graphic hoverGraphic;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+    }
+}
