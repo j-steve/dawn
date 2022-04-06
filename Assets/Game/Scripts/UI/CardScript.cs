8352c357cc5b8a862b5d83b6184bd2ba8aa2c1bc
Stephen <steve5805@gmail.com>
2022-04-05 22:48:34 -0400

    Adds CardContainer UI element

---
+++ Assets/Game/Scripts/UI/CardScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardScript : MonoBehaviour
+{
+
+  [SerializeField] Text cardTitle;
+  [SerializeField] Text cardDescription;
+  [SerializeField] Sprite cardImage;
+
+  // Start is called before the first frame update
+  void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
